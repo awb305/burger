e26f6344969f4865875dd144aa84c7c3d021a7e7
Andrew B <awb305@gmail.com>
2018-08-02 15:13:57 -0400

    progress

--- controller/burgers_controller.js
+++ controller/burgers_controller.js
@@ -10,12 +10,17 @@ let burger =  require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
+    res.render("index");
+  });
+
+
+router.get("/api/burgers", function(req, res) {
   burger.selectAll(function(data) {
     var burgerObject = {
       burger: data
     };
     console.log(burgerObject);
-    res.render("index");
+    res.json(burgerObject);
   });
 });
 
